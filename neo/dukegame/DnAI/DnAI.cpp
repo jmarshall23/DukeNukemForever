0d89848c087efbad78f0e91118d11dc250607b9c
Justin Marshall <justinmarshall20@gmail.com>
2022-07-19 06:57:39 -0700

    Fixed a bug with collision boxes being incorrect because the actor main mesh was in t-pose. We are now using the editor_mins/maxs in the def decls instead, this allows the collision boxes to be user defined which is more correct.

--- neo/dukegame/DnAI/DnAI.cpp
+++ neo/dukegame/DnAI/DnAI.cpp
@@ -60,11 +60,15 @@ void DnAI::SetupPhysics(void)
 
 	fl.solidForTeam = true;
 
-	idVec3 spawnOrigin = GetOrigin() + idVec3(0, 0, 30);
+	idVec3 spawnOrigin = GetOrigin();
 	idClipModel* clipModel = new idClipModel();
 
+	idBounds bounds;
+	bounds[0] = spawnArgs.GetVector("editor_mins");
+	bounds[1] = spawnArgs.GetVector("editor_maxs");
+
 	physicsObj.SetSelf(this);
-	idClipModel* newClip = new idClipModel(idTraceModel(renderEntity.hModel->Bounds()));
+	idClipModel* newClip = new idClipModel(idTraceModel(bounds));
 	newClip->Translate(spawnOrigin);
 	physicsObj.SetClipModel(newClip, 1.0f);
 	physicsObj.SetMass(spawnArgs.GetFloat("mass", "100"));
