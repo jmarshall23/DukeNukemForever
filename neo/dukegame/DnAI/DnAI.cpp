e8229041840c375f9d3c1a31a66434b2cbbdc496
Justin Marshall <justinmarshall20@gmail.com>
2022-07-27 14:20:01 -0700

    Enemies who have no target and have been attacked will now always attack the player.

--- neo/dukegame/DnAI/DnAI.cpp
+++ neo/dukegame/DnAI/DnAI.cpp
@@ -385,13 +385,15 @@ bool DnAI::MoveToPosition(const idVec3& pos)
 
 bool DnAI::Pain(idEntity* inflictor, idEntity* attacker, int damage, const idVec3& dir, int location)
 {
-	if (idMath::FRandRange(1.0f, 10.0f) < 5.0f)
+	bool forceAttack = false;
+	if (target == nullptr)
 	{
-		if (target == nullptr)
-		{
-			target = (idActor *)attacker;
-		}
+		forceAttack = true;
+		target = (idActor*)attacker;
+	}
 
+	if (idMath::FRandRange(1.0f, 10.0f) < 5.0f || forceAttack)
+	{
 		Event_StopMove();
 		Event_SetState("state_ShootEnemy");
 		AI_PAIN = true;
